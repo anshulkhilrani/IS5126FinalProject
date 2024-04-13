f4c3c3c35aaf4c8d05bfd6bcfe4b936b63392028
vizz1234 <34003673+vizz1234@users.noreply.github.com>
2024-04-13 22:34:06 +0800

    Update distil_model.py
    Updated for list of texts

--- distil_model.py
+++ distil_model.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, AdamW
 
-def distil_model(text_list):
+def distil_model(texts):
     num_labels = 4
     tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
     model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=num_labels)
@@ -9,17 +9,18 @@ def distil_model(text_list):
     model.load_state_dict('distil_model.pt')
     model.eval()
     model.to(device)
-    test_text = text_list[:]
-    test_encoding = tokenizer(test_text, truncation=True, padding=True, return_tensors="pt")
+    
+    predictions = []
+    for text in texts:
+        test_encoding = tokenizer(text, truncation=True, padding=True, return_tensors="pt")
+        input_ids = test_encoding['input_ids'].to(device)
+        attention_mask = test_encoding['attention_mask'].to(device)
 
-    input_ids = test_encoding['input_ids'].to(device)
-    attention_mask = test_encoding['attention_mask'].to(device)
+        with torch.no_grad():
+            outputs = model(input_ids, attention_mask=attention_mask)
 
-    model.eval()
-
-    with torch.no_grad():
-        outputs = model(input_ids, attention_mask=attention_mask)
-
-    logits = outputs.logits
-    preds = torch.argmax(logits, axis=1)
-    return preds[0]
+        logits = outputs.logits
+        preds = torch.argmax(logits, axis=1)
+        predictions.append(preds.item())
+    
+    return predictions
